720bce1549868b82e89da9645775cc0ee780973a
Duncanwar <semunda9@gmail.com>
2023-06-14 16:02:08 +0200

    other

--- task/app/scripts/app.js
+++ task/app/scripts/app.js
@@ -10,11 +10,23 @@ async function init() {
 async function renderText() {
   const textElement = document.getElementById('apptext');
   const contactData = await client.data.get('contact');
+
+  const iparams =await client.iparams.get()
+  const {ticket} = await client.data.get('ticket');
+
   const {
     contact: { name }
   } = contactData;
+  var headers = {
+    Authorization: "Basic <%= encode(iparam.api_key) %>",
+    "Content-Type": "application/json",
+  };
+  const result = await client.request.get(`https://${iparams.domain_url}/api/v2/tickets/${ticket.id}/conversations`,{headers:headers})
+  const filteredTicketsConversation = JSON.parse(result.response)
+  
+  console.log(filteredTicketsConversation[filteredTicketsConversation.length - 1])
 
-// Get the modal
+  // Get the modal
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
@@ -23,9 +35,17 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+const modalContent = document.getElementsByClassName('modal-content')[0];
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
+  let textarea = document.createElement("textarea");
+  let button = document.createElement("button");
+  textarea.innerText = filteredTicketsConversation[filteredTicketsConversation.length - 1].body_text;
+  button.onclick = function() {
+    
+  }
+  modalContent.appendChild(textarea);
 }
 
 // When the user clicks on <span> (x), close the modal
